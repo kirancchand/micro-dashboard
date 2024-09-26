f3241ab3099f54e4b3b0bcd6d33b04292f687ec8
kiran c chand <kirancchand@gmail.com>
2024-09-26 17:28:44 +0530

    menu updated to 18 with bootstrap.js changes to other

--- packages/menu/src/bootstrap.js
+++ packages/menu/src/bootstrap.js
@@ -1,57 +1,61 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
-const mount=(el,{onSelectMenu})=>{
-//    el.innerHTML=products;
-   ReactDOM.render(<App onSelectMenu={onSelectMenu}/>,el);
-};
-
-if(process.env.NODE_ENV==="development"){
-    const el=document.querySelector('#_menu-dev-root')
-
-    if(el){
-        mount(el)
-    }
-}
-
-export { mount };
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { createMemoryHistory,createBrowserHistory } from 'history';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createRoot } from 'react-dom/client';
 // import App from './App';
-
-// const mount=(el,{onSelectMenu,onNavigate,defaultHistory,initialPath})=>{
-//     const history = defaultHistory || createMemoryHistory({
-//         initialEntries:[initialPath]
-//     });
-
-//     if(onNavigate){
-//         history.listen(onNavigate);
-//     }
-//     console.log("history",history)
-//     ReactDOM.render(<App onSelectMenu={onSelectMenu} history={history} />,el);
-
-//     return {
-//         onParentNavigate({pathname:nextPathname}){
-//             const {pathname}=history.location;
-//             console.log(nextPathname)
-//             if(pathname!==nextPathname){
-//                 history.push(nextPathname);
-//             }
-//         }
-//     }
+// const mount=(el,{onSelectMenu})=>{
+// //    el.innerHTML=products;
+// const root = createRoot(el); 
+// root.render(<App onSelectMenu={onSelectMenu} />);
+// //    ReactDOM.render(<App onSelectMenu={onSelectMenu}/>,el);
 // };
 
-// ///dev
-// if(process.env.NODE_ENV==='development'){
-//     const devRoot = document.querySelector('#_menu-dev-root')
+// if(process.env.NODE_ENV==="development"){
+//     const devRoot=document.querySelector('#_menu-dev-root')
 
 //     if(devRoot){
-//         mount(devRoot,{defaultHistory:createBrowserHistory()});
+//         mount(devRoot)
 //     }
 // }
 
-// export {mount};
+// export { mount };
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { createMemoryHistory,createBrowserHistory } from 'history';
+import App from './App';
+
+const mount=(el,{onSelectMenu,onNavigate,defaultHistory,initialPath})=>{
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries:[initialPath]
+    });
+
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+   const root = createRoot(el); 
+   root.render(<App onSelectMenu={onSelectMenu} />);   
+
+    return {
+        onParentNavigate({pathname:nextPathname}){
+            const {pathname}=history.location;
+            console.log(nextPathname)
+            if(pathname!==nextPathname){
+                history.push(nextPathname);
+            }
+        }
+    }
+};
+
+///dev
+if(process.env.NODE_ENV==='development'){
+    const devRoot = document.querySelector('#_menu-dev-root')
+
+    if(devRoot){
+        mount(devRoot,{defaultHistory:createBrowserHistory()});
+    }
+}
+
+export {mount};
